bc8cd8a2e251b5dd0e6ff18f741d3cb6906598df
Athish <150988336+Athish0007@users.noreply.github.com>
2025-07-09 17:18:51 +0530

    Delete src directory

--- src/app/components/left-dashboard/left-dashboard.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-left-dashboard',
-  templateUrl: './left-dashboard.component.html',
-  styleUrls: ['./left-dashboard.component.css']
-})
-export class LeftDashboardComponent { }
