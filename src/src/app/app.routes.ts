6839bd3ce8840f2c47dd5c47ee38beae430b390b
Athish <150988336+Athish0007@users.noreply.github.com>
2025-07-20 20:10:55 +0530

    Delete src/src directory

--- src/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { Loginpage } from './pages/loginpage/loginpage';
-import { Home } from './pages/home/home';
-
-export const routes: Routes = [
-  { path: '', component: Loginpage },
-  { path: 'home', component: Home },
-];
